62856b93fa1a005d45dd19936a632a185baa4e60
Gabriel Lima <devlimagabriel@gmail.com>
2021-03-25 01:28:06 -0300

    add test to title componenet

---
+++ src/components/Title/Title.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Title from './index';
+import * as Color from 'styles/colors.module.scss';
+
+const sizes = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+const colors = Object.keys(Color);
+
+it("renders correclty", () => {
+  const { getByText } = render(<Title>teste</Title>);
+  expect(getByText('teste')).toBeTruthy();
+})
+
+it("should have margin 0 when next item is subtitle", () => {
+  const { getByText } = render(<Title subtitle>teste</Title>);
+  expect(getByText('teste')).toHaveStyle('margin-bottom', 0);
+})
+
+sizes.map(size => {
+  it(`should have size ${size}`, () => {
+    const { getByText } = render(<Title size={size}>teste</Title>);
+    expect(getByText('teste')).toHaveClass(size);
+  })
+})
+
+colors.map(color => {
+  it(`should have color ${color}`, () => {
+    const { getByText } = render(<Title color={color}>teste</Title>);
+    expect(getByText('teste')).toHaveClass(`c-${color}`)
+  })
+})
--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './titles.module.scss';
+import styles from './title.module.scss';
 
 const Title = ({ size, color, subtitle, children }) => {
   const Tag = size || 'h1';
-  return <Tag className={`${styles.title} ${styles[color]}`} style={subtitle ? { marginBottom: 0} : {}}>{children}</Tag>
+  return <Tag className={`${styles.title} c-${color}`} style={subtitle ? { marginBottom: 0} : {}}>{children}</Tag>
 };
 
 export default Title;
